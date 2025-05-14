2803db5ecafb39bb0dea8af7a0f9bfe27885a3f5
ekow <ekow.a.indome@gmail.com>
2025-05-14 16:12:26 +0000

    disabling explore all features temporarily

--- client/src/lib/components/landing/Features.svelte
+++ client/src/lib/components/landing/Features.svelte
@@ -154,15 +154,14 @@
     </div>
     
     <!-- "Explore All Features" button -->
-    <div class="mt-12 text-center">
-      <!-- svelte-ignore a11y_invalid_attribute -->
+    <!-- <div class="mt-12 text-center">
       <a href="#" class="inline-flex items-center justify-center py-3 px-6 rounded-full bg-[#1a5f4a] text-white hover:bg-[#1a5f4a]/90 transition-all duration-300 shadow-sm hover:shadow-md group">
         <span>Explore All Features</span>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2 transition-transform duration-300 group-hover:translate-x-1" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
       </a>
-    </div>
+    </div> -->
   </div>
 </section>
 
