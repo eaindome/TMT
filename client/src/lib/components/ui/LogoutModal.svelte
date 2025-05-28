1c7d3a066852aca60a07066b27bf931f1104b85e
ekow <ekow.a.indome@gmail.com>
2025-05-28 11:32:39 +0000

    Removing the logout svg

--- client/src/lib/components/ui/LogoutModal.svelte
+++ client/src/lib/components/ui/LogoutModal.svelte
@@ -36,12 +36,6 @@
     >
       <div class="p-5">
         <div class="flex flex-col items-center text-center">
-          <div class="mb-4 rounded-full bg-red-100 p-3">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-            </svg>
-          </div>
-          
           <h3 class="mb-2 text-xl font-medium text-gray-900">Log out</h3>
           <p class="mb-6 text-gray-600">Are you sure you want to log out of your account?</p>
           
